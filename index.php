3bdacee39a13ed24dcfe9fccb58b3be8d1abe68d
TimBSS1985 <109221778+TimBSS1985@users.noreply.github.com>
2024-06-02 14:44:39 +0200

    database connectie

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php echo "welkom jij daar! :)" ?>
+<?php
+$servername = "localhost";
+$username = "tim";
+$password = "test123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
